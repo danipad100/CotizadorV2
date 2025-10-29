7328f6b8c3f3b84f5f1296a44dc9557de84f6903
danipad100 <danipad100@gmail.com>
2025-10-29 11:49:41 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,6 @@
+const VERSION = '2';
+self.addEventListener('install', e=>{ self.skipWaiting(); });
+self.addEventListener('activate', e=>{ clients.claim(); });
+self.addEventListener('fetch', e=>{
+  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+});
